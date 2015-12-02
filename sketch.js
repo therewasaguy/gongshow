24efc923edfd375ec9a5f27c40016b5d58908894
therewasaguy <jason.sigal@gmail.com>
2015-12-02 14:31:05 -0500

    init gongshow

---
+++ sketch.js
@@ -0,0 +1,67 @@
+// how much time?
+var countdownMax = 1;
+var countdown = countdownMax;
+var lastStart = 0;
+var counting = false;
+var startButton;
+var stopButton;
+var timeSpan;
+
+var gongPaths = [
+  'audio/23580__loofa__gong1.mp3',
+  'audio/23580__loofa__gong1.mp3',
+  'audio/23580__loofa__gong1.mp3',
+  'audio/23580__loofa__gong1.mp3',
+  'audio/23580__loofa__gong1.mp3'
+];
+
+var gongPlayers = [];
+var gongIterator = 0;
+
+function setup() {
+  noCanvas();
+
+  timeSpan = createElement('span');
+  createElement('div');
+  startButton = createButton('start');
+  stopButton = createButton('stop');
+  startButton.mousePressed(startTimer);
+  stopButton.mousePressed(stopTimer);
+  
+  // initialize the gong players
+  for (var i = 0; i < gongPaths.length; i++) {
+    gongPlayers[i] = createAudio(gongPaths[i]);
+  }
+
+}
+
+function draw() {
+  timeSpan.html(countdown.toFixed(2));
+
+  if (counting) {
+    countdown = (countdownMax*1000 + lastStart - millis()) / 1000;
+  }
+
+  if (countdown <= 0) {
+    playGong();
+    stopTimer();
+  }
+}
+
+function startTimer() {
+  counting = true;
+  lastStart = millis();
+}
+
+function stopTimer() {
+  counting = false;
+  countdown = countdownMax;
+}
+
+function playGong() {
+  var nextGong = gongPlayers[gongIterator];
+  nextGong.play();
+  gongIterator = (gongIterator + 1) % gongPlayers.length
+  console.log(gongIterator);
+  
+}
