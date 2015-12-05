2451a096eac4cdcd5c35c3d0ee65bd3553fac8ed
therewasaguy <jason.sigal@gmail.com>
2015-12-05 10:12:13 -0500

    video mode instead of audio

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 // how much time?
-var countdownMax = 30;
+var countdownMax = 57;
 var countdown = countdownMax;
 var lastStart = 0;
 var counting = false;
@@ -7,36 +7,47 @@ var startButton;
 var stopButton;
 var timeSpan;
 
-var gongPaths = [
-  'audio/23580__loofa__gong1.mp3',
-  'audio/23580__loofa__gong1.mp3',
-  'audio/23580__loofa__gong1.mp3',
-  'audio/23580__loofa__gong1.mp3',
-  'audio/23580__loofa__gong1.mp3'
-];
+var gongVidLoop;
+var gongVidStrike;
 
-var gongPlayers = [];
-var gongIterator = 0;
+// var gongPaths = [
+//   'audio/23580__loofa__gong1.mp3',
+//   'audio/23580__loofa__gong1.mp3',
+//   'audio/23580__loofa__gong1.mp3',
+//   'audio/23580__loofa__gong1.mp3',
+//   'audio/23580__loofa__gong1.mp3'
+// ];
+
+// var gongPlayers = [];
+// var gongIterator = 0;
 
 function setup() {
   noCanvas();
 
-  timeSpan = createElement('span');
-  createElement('div');
-  startButton = createButton('start');
-  stopButton = createButton('stop');
-  startButton.mousePressed(startTimer);
-  stopButton.mousePressed(stopTimer);
+  // set up video
+  gongVidLoop = createVideo('video/gong_loop_01.mp4');
+  gongVidStrike = createVideo('video/gong_strike_2.mp4');
+  gongVidStrike.hide();
+  gongVidLoop.position(0, 0);
+  gongVidLoop.size(windowWidth, windowHeight);
+  gongVidStrike.size(windowWidth, windowHeight);
+
+  // timeSpan = createElement('span');
+  // createElement('div');
+  // startButton = createButton('start');
+  // stopButton = createButton('stop');
+  // startButton.mousePressed(startTimer);
+  // stopButton.mousePressed(stopTimer);
   
   // initialize the gong players
-  for (var i = 0; i < gongPaths.length; i++) {
-    gongPlayers[i] = createAudio(gongPaths[i]);
-  }
+  // for (var i = 0; i < gongPaths.length; i++) {
+  //   gongPlayers[i] = createAudio(gongPaths[i]);
+  // }
 
 }
 
 function draw() {
-  timeSpan.html(countdown.toFixed(2));
+  // timeSpan.html(countdown.toFixed(2));
 
   if (counting) {
     countdown = (countdownMax*1000 + lastStart - millis()) / 1000;
@@ -49,19 +60,44 @@ function draw() {
 }
 
 function startTimer() {
+  console.log('start timer');
   counting = true;
   lastStart = millis();
+  gongVidLoop.show();
+  gongVidStrike.hide();
+  gongVidLoop.loop();
 }
 
 function stopTimer() {
+  console.log('stop timer');
   counting = false;
   countdown = countdownMax;
+
+  gongVidStrike.show();
+  gongVidLoop.hide();
+  gongVidStrike.time(0);
+  gongVidStrike.play();
+  // gongVidLoop.pause();
 }
 
 function playGong() {
-  var nextGong = gongPlayers[gongIterator];
-  nextGong.play();
-  gongIterator = (gongIterator + 1) % gongPlayers.length
-  console.log(gongIterator);
-  
+  return;
+  // var nextGong = gongPlayers[gongIterator];
+  // nextGong.play();
+  // gongIterator = (gongIterator + 1) % gongPlayers.length
+  // console.log(gongIterator);
+}
+
+// space bar to start timer, z to stop
+function keyPressed() {
+  if (key == ' ') {
+    startTimer();
+  } else if (key == 'Z') {
+    stopTimer();
+  }
+}
+
+function windowResized() {
+  gongVidLoop.size(windowWidth, windowHeight);
+  gongVidStrike.size(windowWidth, windowHeight);
 }
