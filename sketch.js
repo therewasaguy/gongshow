fcb143f87afc144f9ff7fe382c9f1c48bf349019
therewasaguy <jason.sigal@gmail.com>
2015-12-05 10:30:05 -0500

    cleanup

--- sketch.js
+++ sketch.js
@@ -1,25 +1,17 @@
+/**
+ *  p - pause
+ *  spacebar - start countdown
+ *  z - trigger gong
+ */
+
 // how much time?
 var countdownMax = 57;
 var countdown = countdownMax;
 var lastStart = 0;
 var counting = false;
-var startButton;
-var stopButton;
-var timeSpan;
 
 var gongVidLoop;
 var gongVidStrike;
-
-// var gongPaths = [
-//   'audio/23580__loofa__gong1.mp3',
-//   'audio/23580__loofa__gong1.mp3',
-//   'audio/23580__loofa__gong1.mp3',
-//   'audio/23580__loofa__gong1.mp3',
-//   'audio/23580__loofa__gong1.mp3'
-// ];
-
-// var gongPlayers = [];
-// var gongIterator = 0;
 
 function setup() {
   noCanvas();
@@ -32,30 +24,16 @@ function setup() {
   gongVidLoop.size(windowWidth, windowHeight);
   gongVidStrike.size(windowWidth, windowHeight);
 
-  // timeSpan = createElement('span');
-  // createElement('div');
-  // startButton = createButton('start');
-  // stopButton = createButton('stop');
-  // startButton.mousePressed(startTimer);
-  // stopButton.mousePressed(stopTimer);
-  
-  // initialize the gong players
-  // for (var i = 0; i < gongPaths.length; i++) {
-  //   gongPlayers[i] = createAudio(gongPaths[i]);
-  // }
-
 }
 
 function draw() {
-  // timeSpan.html(countdown.toFixed(2));
-
   if (counting) {
     countdown = (countdownMax*1000 + lastStart - millis()) / 1000;
   }
 
   if (countdown <= 0) {
-    playGong();
     stopTimer();
+    playGong();
   }
 }
 
@@ -72,28 +50,28 @@ function stopTimer() {
   console.log('stop timer');
   counting = false;
   countdown = countdownMax;
-
-  gongVidStrike.show();
-  gongVidLoop.hide();
-  gongVidStrike.time(0);
-  gongVidStrike.play();
   // gongVidLoop.pause();
 }
 
 function playGong() {
-  return;
-  // var nextGong = gongPlayers[gongIterator];
-  // nextGong.play();
-  // gongIterator = (gongIterator + 1) % gongPlayers.length
-  // console.log(gongIterator);
+  gongVidStrike.show();
+  gongVidLoop.hide();
+  gongVidStrike.time(0);
+  gongVidStrike.play();
 }
 
 // space bar to start timer, z to stop
 function keyPressed() {
   if (key == ' ') {
     startTimer();
-  } else if (key == 'Z') {
+  }
+  else if (key == 'Z') {
     stopTimer();
+    playGong();
+  }
+  else if (key == 'P') {
+    counting = false;
+    gongVidLoop.pause();
   }
 }
 
